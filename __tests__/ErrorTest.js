ee24008a763c6397f729772a7507ee59b112e9fb
Hyelim Choi <HyelimChoi01@gmail.com>
2023-12-16 16:48:12 +0900

    test: test of start day validator

---
+++ __tests__/ErrorTest.js
@@ -0,0 +1,88 @@
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { EOL as LINE_SEPARATOR } from "os";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    if (input === undefined) {
+      throw new Error("NO INPUT");
+    }
+
+    return Promise.resolve(input);
+  });
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join(LINE_SEPARATOR);
+};
+
+const expectLogContains = (received, expects) => {
+  expects.forEach((exp) => {
+    expect(received).toContain(exp);
+  });
+};
+
+const runExceptions = async ({
+  inputs = [],
+  retryInputs = [],
+  expected = [],
+}) => {
+  // given
+  const logSpy = getLogSpy();
+  mockQuestions([...inputs, ...retryInputs]);
+
+  // when
+  const app = new App();
+  await app.run();
+
+  // then
+  expectLogContains(getOutput(logSpy), expected);
+};
+
+const run = async ({ inputs = [], expected = [] }) => {
+  // given
+  const logSpy = getLogSpy();
+  mockQuestions(inputs);
+
+  // when
+  const app = new App();
+  await app.run();
+
+  expectLogContains(getOutput(logSpy), expected);
+};
+
+describe("예외 테스트", () => {
+  test("배정 시작 요일 한글자 예외 테스트", async () => {
+    await runExceptions({
+      inputs: ["4,토요일"],
+      retryInputs: [
+        "4,토",
+        "허브,쥬니,말랑,라온,헤나,우코,에단,수달,파워,히이로,마코,슬링키,모디,연어,깃짱,리오,고니,박스터,달리,조이,노아이즈,도이,도치,홍고,스캇,폴로,해시,로지,첵스,아이크,우가,푸만능,애쉬,로이스,오션",
+        "오션,로이스,애쉬,푸만능,우가,아이크,첵스,로지,해시,폴로,스캇,홍고,도치,도이,노아이즈,조이,달리,박스터,고니,리오,깃짱,연어,모디,슬링키,마코,히이로,파워,수달,에단,우코,헤나,라온,말랑,쥬니,허브",
+      ],
+      expected: ["[ERROR]", "요일은 한 글자의 값이어야 합니다."],
+    });
+  });
+
+  test("배정 시작 요일 유효값 예외 테스트", async () => {
+    await runExceptions({
+      inputs: ["4,롸"],
+      retryInputs: [
+        "4,토",
+        "허브,쥬니,말랑,라온,헤나,우코,에단,수달,파워,히이로,마코,슬링키,모디,연어,깃짱,리오,고니,박스터,달리,조이,노아이즈,도이,도치,홍고,스캇,폴로,해시,로지,첵스,아이크,우가,푸만능,애쉬,로이스,오션",
+        "오션,로이스,애쉬,푸만능,우가,아이크,첵스,로지,해시,폴로,스캇,홍고,도치,도이,노아이즈,조이,달리,박스터,고니,리오,깃짱,연어,모디,슬링키,마코,히이로,파워,수달,에단,우코,헤나,라온,말랑,쥬니,허브",
+      ],
+      expected: ["[ERROR]", "요일은 일월화수목금토 중 하나의 값이어야 합니다."],
+    });
+  });
+});
